4d600d7b1081bda669ca87977d84f7ab717c76b8
이노디스 이종원 <moguri61@innodis.co.kr>
2020-12-23 02:02:53 +0900

    회원가입페이지 완료

---
+++ resources/js/common.js
@@ -0,0 +1,50 @@
+$(document).ready(function () {
+	// 스크롤 바로이동
+	$(window).scroll(function () {
+		if ($(this).scrollTop() > 200) {
+			$("#topPage").fadeIn();
+		} else {
+			$("#topPage").fadeOut();
+		}
+	});
+
+	// scroll body to 0px on click
+	$("#btnTop").bind("click", function (e) {
+		// Prevents the default action to be triggered.
+		e.preventDefault();
+
+		$("body,html").animate(
+			{
+				scrollTop: 0,
+			},
+			500
+		);
+	});
+
+	// 스크롤 바로이동
+	$(".anchor").click(function (e) {
+		// Prevents the default action to be triggered.
+		e.preventDefault();
+
+		$("html,body").animate(
+			{
+				scrollTop: $(this.hash).offset().top - 120,
+			},
+			500
+		);
+	});
+
+	// gnb
+	$("#btGnb, #btGnbClose, .fixmasking").bind("click", function (e) {
+		e.preventDefault();
+		let allMenu = $("#wrapper");
+
+		if (allMenu.hasClass("gnbopen") === false) {
+			$("body").addClass("noscroll");
+			allMenu.addClass("gnbopen");
+		} else {
+			$("body").removeClass("noscroll");
+			allMenu.removeClass("gnbopen");
+		}
+	});
+});
